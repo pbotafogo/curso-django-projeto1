73c489506a241d52f0a26dbe9b00108f9b95ae7b
Pedro Baumgarten Botafogo <pedrobbotafogo@gmail.com>
2025-09-09 16:59:36 -0300

    Atualização do Admin, Criação das Páginas e Inclusão dos Models

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,17 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Pedro Baumgarten Botafogo',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
-def govbr(request):
-    return render(request, 'recipes/pages/govbr.html', context={
-        'name': 'Pedro Baumgarten Botafogo',
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
