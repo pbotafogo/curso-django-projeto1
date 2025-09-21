33f659b9e6cd8c0828bb440954e475c31d3ccd04
Pedro Baumgarten Botafogo <pedrobbotafogo@gmail.com>
2025-09-21 10:13:05 -0300

    Model Update

--- recipes/models.py
+++ recipes/models.py
@@ -12,7 +12,6 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
